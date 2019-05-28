fef2f69b8bd9b7cd3d8f48aa6db58f5202f23283
Sandesh S <sandesh.sign@gmail.com>
2019-05-29 00:13:41 +0530

    Submitted solution for '43_Multiply_Strings'

---
+++ src/com/sign/problems/MultiplyStrings_43/PrintStreamExample.java
@@ -0,0 +1,24 @@
+package com.sign.problems.MultiplyStrings_43;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class PrintStreamExample {
+    public String multiply(String num1, String num2) {
+        int n1=0 ,n2=0;
+
+        for ( int i = 0; i < num1.length(); i++ ){
+            int digit = num1.charAt(i) - 48;
+            n1 = n1*10 + digit;
+        }
+        for ( int i = 0; i < num2.length(); i++ ){
+            int digit = num2.charAt(i) - 48;
+            n2 = n2*10 + digit;
+        }
+        System.out.println(n1+ " "+ n2);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos, true);
+        System.setOut(ps);
+        System.out.print(n1*n2);
+        return baos.toString();
+    }
+}
---
+++ src/com/sign/problems/MultiplyStrings_43/Solution.java
@@ -0,0 +1,51 @@
+package com.sign.problems.MultiplyStrings_43;
+
+
+/*
+* Sandesh S 29-May-2019
+*
+* O(n2) Traditional multiplication
+*
+* Beats 100% in memory consumption
+*
+* */
+
+import java.util.Arrays;
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        int result[] = new int[num1.length()+num2.length()];
+        Arrays.fill(result, 0);
+        int resultIndex = result.length-1;
+
+        if (num1.length() == 1 && num1.charAt(0) == '0')
+            return "0";
+
+        if (num2.length() == 1 && num2.charAt(0) == '0')
+            return "0";
+
+        for (int i=num2.length()-1; i>=0; i--) {
+            int k = resultIndex;
+            int iValue = num2.charAt(i)-48;
+            int carry=0;
+
+            for (int j=num1.length()-1; j>=0; j--) {
+                int jValue = num1.charAt(j)-48;
+                int product = (iValue*jValue) + carry + result[k];
+                result[k] = product % 10;
+                carry = product / 10;
+                k--;
+            }
+            resultIndex--;
+
+            if (carry != 0)
+                result[k] = carry;
+        }
+
+        String finalResult = Arrays.toString(result).replace("[", "").replace("]", "").replace(",", "").replace(" ", "");
+        if (result[0] != 0)
+            return finalResult;
+        else
+            return finalResult.substring(1);
+    }
+}
