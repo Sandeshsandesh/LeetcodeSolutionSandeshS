39584a0c38b9d3825b8c93079f8e75a51456ec19
Sandesh S <sandesh.s@dell.com>
2022-09-02 13:44:47 +0530

    Updated solution of 852 with a Simple approach - Binary search over comparison

--- src/com/sign/problems/peakindexmountain_852/PeakIndexInMountainArray.java
+++ src/com/sign/problems/peakindexmountain_852/PeakIndexInMountainArray.java
@@ -1,6 +1,11 @@
 package com.sign.problems.peakindexmountain_852;
 
 public class PeakIndexInMountainArray {
+    /**
+     * logn
+     * @param arr
+     * @return
+     */
     public int peakIndexInMountainArray(int[] arr) {
         int low = 0;
         int high = arr.length - 1;
@@ -19,5 +24,28 @@ public class PeakIndexInMountainArray {
             }
         }
         return -1;
+    }
+
+    /**
+     * Simple approach with logn
+     * Comparison: a[i] < a[i+1]
+     * TTTTFF (find first F)
+     * @param arr
+     * @return
+     */
+    public int binarySearchOverComparisonsApproachPeakIndexInMountainArray(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] < arr[mid+1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
     }
 }
